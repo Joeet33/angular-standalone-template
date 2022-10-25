b0162221a3c2677ddc7a2b1c259f8820f2350e42
Joe Ellul-Turner <joeet@pop-os.localdomain>
2022-10-25 21:15:52 +0100

    layout component

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutComponent } from 'src/components/layout/layout.component';
+import { InfoListComponent } from 'src/components/info-list/info-list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LayoutComponent, InfoListComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
